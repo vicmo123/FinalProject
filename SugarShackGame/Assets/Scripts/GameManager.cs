6b767ada802ca3424ecacba48af2c0e035b3932c
Léonie St-Onge <leonie.stonge@gmail.com>
2023-04-13 13:08:14 -0400

    GameManager Script

---
+++ SugarShackGame/Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : IFlow
+{
+    public void PreInitialize()
+    {
+    }
+    public void Initialize()
+    {
+    }
+
+    public void PhysicsRefresh()
+    {
+    }
+
+
+    public void Refresh()
+    {
+    }
+
+
+}
--- SugarShackGame/Assets/Scripts/MainEntry.cs
+++ SugarShackGame/Assets/Scripts/MainEntry.cs
@@ -4,24 +4,25 @@ using UnityEngine;
 
 public class MainEntry : MonoBehaviour
 {
-    //GameManager gameManager;
+    GameManager gameManager;
     private void Awake()
     {
-        
+        gameManager = new GameManager();
+        gameManager.PreInitialize();
     }
     void Start()
     {
-        
+        gameManager.Initialize();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        gameManager.Refresh();
     }
     private void FixedUpdate()
     {
-        
+        gameManager.PhysicsRefresh();
     }
     
 }
