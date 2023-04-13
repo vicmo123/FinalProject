2337e7d1b378e3460f1751f95a4d7f5c93e72e09
Léonie St-Onge <leonie.stonge@gmail.com>
2023-04-13 13:31:48 -0400

    AbilityManager linked in the GameManager

--- SugarShackGame/Assets/Scripts/GameManager.cs
+++ SugarShackGame/Assets/Scripts/GameManager.cs
@@ -4,20 +4,24 @@ using UnityEngine;
 
 public class GameManager : IFlow
 {
+
     public void PreInitialize()
     {
+        AbilityManager.Instance().PreInitialize();
     }
     public void Initialize()
     {
+        AbilityManager.Instance().Initialize();
     }
 
     public void PhysicsRefresh()
     {
+        AbilityManager.Instance().PhysicsRefresh();
     }
-
 
     public void Refresh()
     {
+        AbilityManager.Instance().Refresh();
     }
 
 
