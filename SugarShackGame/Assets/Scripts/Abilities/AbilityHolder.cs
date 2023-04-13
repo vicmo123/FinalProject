2b6e759a23cf40b2229f588db8c761d369f6e446
Léonie St-Onge <leonie.stonge@gmail.com>
2023-04-13 13:42:42 -0400

    AbilityHolder Script

---
+++ SugarShackGame/Assets/Scripts/Abilities/AbilityHolder.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbilityHolder : MonoBehaviour, IFlow
+{
+    public void PreInitialize()
+    {
+    }
+
+    public void Initialize()
+    {
+    }
+
+    public void PhysicsRefresh()
+    {
+    }
+
+     public void Refresh()
+    {
+    }
+}
