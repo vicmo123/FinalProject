91181ae4910a8747c267f272aa5e0822309f0e49
Léonie St-Onge <leonie.stonge@gmail.com>
2023-04-13 13:03:30 -0400

    MainEntry Script

---
+++ SugarShackGame/Assets/Scripts/MainEntry.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainEntry : MonoBehaviour
+{
+    //GameManager gameManager;
+    private void Awake()
+    {
+        
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void FixedUpdate()
+    {
+        
+    }
+    
+}
