11c56e4d1d56da0bb7bf9072ae172a41966790bd
vicmo123 <vicmo26@gmail.com>
2023-04-18 15:01:57 -0400

    Two players can appear on screen

--- SugarShackGame/Assets/Scripts/Player/PlayerManager.cs
+++ SugarShackGame/Assets/Scripts/Player/PlayerManager.cs
@@ -29,8 +29,7 @@ public class PlayerManager : IFlow
 
     PlayerInputManager playerInputManager;
     PlayerFactory factory;
-    Player player1;
-    Player player2;
+    public List<Player> players { get; private set; }
 
     //Temp for demo
     int currentBeardIndex = 0;
@@ -38,69 +37,67 @@ public class PlayerManager : IFlow
 
     public void PreInitialize()
     {
+        players = new List<Player>();
+
         playerInputManager = GameObject.FindGameObjectWithTag("GameController").GetComponent<PlayerInputManager>();
         playerInputManager.playerJoinedEvent.AddListener((val) => {
-            val.gameObject.transform.position = Vector3.zero;
-            player1 = val.gameObject.GetComponent<Player>();
-            factory.ChangePlayerColor(ref player1, factory.beardColors[currentBeardIndex], factory.shirtColors[currentShirtIndex]);
-            player1.PreInitialize();
-            //player2.PreInitialize();
+            Player newPlayer = new Player();
 
-            player1.Initialize();
-            //player2.Initialize();
+            val.gameObject.transform.position = Vector3.zero;
+            newPlayer = val.gameObject.GetComponent<Player>();
+            factory.ChangePlayerColor(ref newPlayer, factory.beardColors[currentBeardIndex], factory.shirtColors[currentShirtIndex]);
+            
+            newPlayer.PreInitialize();
+
+            newPlayer.Initialize();
+
+            players.Add(newPlayer);
         });
+
         factory = new PlayerFactory();
     }
 
     public void Initialize()
     {
-        //AddPlayers();
+        
     }
 
     public void Refresh()
     {
-        player1.Refresh();
-        //player2.Refresh();
-
-        //For demo
-        if (Input.GetKeyDown(KeyCode.V))
+        foreach (var player in players)
         {
-            currentBeardIndex++;
-            if (currentBeardIndex >= factory.beardColors.Length)
-            {
-                currentBeardIndex = 0;
-            }
-
-            factory.ChangePlayerColor(ref player1, factory.beardColors[currentBeardIndex], factory.shirtColors[currentShirtIndex]);
+            player.Refresh();
         }
 
-        if (Input.GetKeyDown(KeyCode.C))
-        {
-            currentShirtIndex++;
-            if (currentShirtIndex >= factory.shirtColors.Length)
-            {
-                currentShirtIndex = 0;
-            }
+        ////For demo
+        //if (Input.GetKeyDown(KeyCode.V))
+        //{
+        //    currentBeardIndex++;
+        //    if (currentBeardIndex >= factory.beardColors.Length)
+        //    {
+        //        currentBeardIndex = 0;
+        //    }
 
-            factory.ChangePlayerColor(ref player1, factory.beardColors[currentBeardIndex], factory.shirtColors[currentShirtIndex]);
-        }
+        //    factory.ChangePlayerColor(ref player1, factory.beardColors[currentBeardIndex], factory.shirtColors[currentShirtIndex]);
+        //}
+
+        //if (Input.GetKeyDown(KeyCode.C))
+        //{
+        //    currentShirtIndex++;
+        //    if (currentShirtIndex >= factory.shirtColors.Length)
+        //    {
+        //        currentShirtIndex = 0;
+        //    }
+
+        //    factory.ChangePlayerColor(ref player1, factory.beardColors[currentBeardIndex], factory.shirtColors[currentShirtIndex]);
+        //}
     }
 
     public void PhysicsRefresh()
     {
-        player1.PhysicsRefresh();
-        //player2.PhysicsRefresh();
-    }
-
-    public void AddPlayers()
-    {
-        player1 = factory.CreatPlayer(factory.beardColors[currentBeardIndex], factory.shirtColors[currentShirtIndex]);
-        //player2 = factory.CreatPlayer(factory.beardColorList[1], factory.shirtColorList[0]);
-
-        player1.PreInitialize();
-        //player2.PreInitialize();
-
-        player1.Initialize();
-        //player2.Initialize();
+        foreach (var player in players)
+        {
+            player.Refresh();
+        }
     }
 }
