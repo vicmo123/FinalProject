6d0d70a8b17adce526ebf55a03f5c11f2bc5ed6b
Léonie St-Onge <leonie.stonge@gmail.com>
2023-04-30 17:21:10 -0400

    Refactoring + Saving GameData

---
+++ SugarShackGame/Assets/Scripts/UI/Views/PlayerViewport.cs
@@ -0,0 +1,111 @@
+using UnityEngine.UI;
+using UnityEngine;
+using TMPro;
+using UnityEngine.InputSystem;
+
+public class PlayerViewport : MonoBehaviour
+{
+    public Transform spawn;
+    public Button leftArrowBtn;
+    public Button rightArrowBtn;
+    public Image connectImg;
+    public TMP_Text text;
+    public Image deviceImg;
+    public Image readyImg;
+    private Player player;
+    private Sprite[] devicesSprites;
+    private Sprite[] readySprites;
+    public double startTime = 0;
+    private double holdDuration = 3f;
+    bool actionOver = false;
+    private bool isReady = false;
+    
+    private void LoadResources()
+    {
+        devicesSprites = Resources.LoadAll<Sprite>("Sprites/UI_Views/Devices");
+        foreach (var item in devicesSprites)
+        {
+            Debug.Log(item);
+        }
+        readySprites = Resources.LoadAll<Sprite>("Sprites/UI_Views/Ready");
+    }
+    public void LinkPlayer(Player player)
+    {
+        LoadResources();
+
+        this.player = player;
+        PlayerInput input = player.gameObject.GetComponent<PlayerInput>();
+        if (input.user.pairedDevices.Count > 0)
+        {
+            foreach (var item in input.user.pairedDevices)
+            {
+                Debug.Log(item.device.name);
+
+                if (item.device.name == DeviceType.Keyboard.ToString())
+                {
+                    DisplayConnecteMsg(DeviceType.Keyboard);                    
+                }
+                else if (item.device.name == DeviceType.Mouse.ToString())
+                {
+                    DisplayConnecteMsg(DeviceType.Keyboard);
+                }
+                else if (item.device.name == DeviceType.Controller.ToString())
+                {
+                    DisplayConnecteMsg(DeviceType.Controller);
+                }
+            }
+        }
+        readyImg.sprite = readySprites[1];
+    }
+
+    public void DisplayConnecteMsg(DeviceType type)
+    {
+        connectImg.color = new Color(0.237f, 0.99f, 0.088f, 0.63f);
+        text.text = "Connected";
+
+        DisplayDeviceImg(type);
+    }
+
+    public void DisplayDeviceImg(DeviceType type)
+    {
+        switch (type)
+        {
+            case DeviceType.Controller:
+                deviceImg.sprite = devicesSprites[0];
+                break;
+            case DeviceType.Keyboard:
+                deviceImg.sprite = devicesSprites[1];
+                break;
+            case DeviceType.Mouse:
+                deviceImg.sprite = devicesSprites[1];
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void DisplayReady()
+    {
+        readyImg.sprite = readySprites[1];
+        isReady = true;
+    }
+
+    public bool IsReady()
+    {
+        return isReady;
+    }
+
+    public void FeedbackArrow(DirectionType direction)
+    {
+
+    }
+    public void LeftArrowAction()
+    {
+
+    }
+
+    public void RightArrowAction()
+    {
+
+    }
+}
--- SugarShackGame/Assets/Scripts/UI/Views/SetupView.cs
+++ SugarShackGame/Assets/Scripts/UI/Views/SetupView.cs
@@ -6,12 +6,20 @@ using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.Layouts;
 using UnityEngine.UI;
 public enum DirectionType { Up, Down, Left, Right };
+public enum DeviceType { Controller, Keyboard, Mouse};
 public enum PlayerID { zero, First, Second };
 public class SetupView : MonoBehaviour
 {
+    private bool DEBUG_MODE = true;
+
+    Dictionary<Player, PlayerViewport> dict;
     public Canvas nextCanvas;
     private PlayerFactory factory;
     private PlayerControls actions;
+
+    public PlayerViewport viewport1;
+    public PlayerViewport viewport2;
+
 
     #region Player
     private PlayerGameData[] players;
@@ -22,10 +30,10 @@ public class SetupView : MonoBehaviour
     private Player p2;
     private string[] beards;
     private string[] shirts;
-    private int P1CurrentBeard = 0;
-    private int P2CurrentBeard = 1;
-    private int P1CurrentShirt = 0;
-    private int P2CurrentShirt = 3;
+    //private int P1CurrentBeard = 0;
+    //private int P2CurrentBeard = 1;
+    //private int P1CurrentShirt = 0;
+    //private int P2CurrentShirt = 3;
     private bool p1Connected = false;
     private bool p2Connected = false;
     #endregion
@@ -44,7 +52,7 @@ public class SetupView : MonoBehaviour
     #region ConnectedImg
     public Image[] connectImg;
     public TMP_Text[] texts;
-    public Image[] devices;
+    public Image[] devicesImg;
     private Sprite[] devicesSprites;
     #endregion
 
@@ -54,69 +62,20 @@ public class SetupView : MonoBehaviour
         //REMOVE AFTER TESTING :
         UIManager.Instance.Initialize();
         //-------------------------------
-        factory = new PlayerFactory("Prefabs/Player/PlayerDemo");
-        beards = factory.beardColors;
-        shirts = factory.shirtColors;
+      
+        SetUIResources();
         LoadPlayers();
-        LoadImages();
         InitActions();
     }
 
-    private void Submit_started(InputAction.CallbackContext obj)
+    private void SetUIResources()
     {
-        actionOver = false;
-        startTime = obj.startTime;
+        factory = new PlayerFactory("Prefabs/Player/PlayerDemo");
+        beards = factory.beardColors;
+        shirts = factory.shirtColors;
     }
 
-    private void Submit_performed(InputAction.CallbackContext obj)
-    {
-        actionOver = true;
-        Debug.Log("Submit was held for :" + obj.duration + " duration");
-        if (obj.control.device.deviceId == (int)PlayerID.First && obj.duration >= holdDuration)
-        {
-            connectImg[0].color = new Color(0.237f, 0.99f, 0.088f, 0.63f);
-            texts[0].text = "Connected";
-
-            if (obj.control.device.name != "Keyboard")
-            {
-                //Sprite : controller appears for conection
-                devices[0].sprite = devicesSprites[0];
-            }
-            else
-            {
-                //Sprite : keyboard appears for conection
-                devices[0].sprite = devicesSprites[1];
-            }
-        }
-
-        if (obj.control.device.deviceId == (int)PlayerID.Second && obj.duration <= holdDuration)
-        {
-            connectImg[1].color = new Color(0.87f, 0.00f, 0.98f, 0.01f);
-            connectImg[1].GetComponentInChildren<TextMeshPro>().text = "Connected";
-        }
-
-        SaveColors();
-        GoToNextPage();
-    }
-    private void SaveColors()
-    {
-        for (int i = 0; i < UIManager.Instance.players.Length; i++)
-        {
-            UIManager.Instance.players[i].indexBeard = players[i].indexBeard;
-            UIManager.Instance.players[i].indexShirt = players[i].indexShirt;
-        }
-    }
-
-    private void Right_performed(InputAction.CallbackContext obj)
-    {
-        SwitchColor(obj.control.device.deviceId, DirectionType.Right);
-    }
-
-    private void Left_performed(InputAction.CallbackContext obj)
-    {
-        SwitchColor(obj.control.device.deviceId, DirectionType.Left);
-
-    }
+        
     private void SwitchColor(int id, DirectionType type)
     {
         switch (type)
@@ -140,47 +99,9 @@ public class SetupView : MonoBehaviour
         }
         Player player = (id == (int)PlayerID.First) ? p1 : p2;
         StartCoroutine(SelectEffect(player));
-
     }
 
-    public void P1RightArrow()
-    {
-        ++P1CurrentShirt;
-        P1CurrentShirt %= shirts.Length;
-        factory.ChangePlayerColor(ref p1, beards[P1CurrentBeard], shirts[P1CurrentShirt]);
-        StartCoroutine(SelectEffect(p1));
-    }
-
-    public void P1LeftArrow()
-    {
-        --P1CurrentShirt;
-        if (P1CurrentShirt == -1)
-        {
-            P1CurrentShirt = shirts.Length - 1;
-        }
-        factory.ChangePlayerColor(ref p1, beards[P1CurrentBeard], shirts[P1CurrentShirt]);
-        StartCoroutine(SelectEffect(p1));
-    }
-
-    public void P2RightArrow()
-    {
-        ++P2CurrentShirt;
-        P2CurrentShirt %= shirts.Length;
-        factory.ChangePlayerColor(ref p2, beards[P2CurrentBeard], shirts[P2CurrentShirt]);
-        StartCoroutine(SelectEffect(p2));
-    }
-
-    public void P2LeftArrow()
-    {
-        --P2CurrentShirt;
-        if (P2CurrentShirt == -1)
-        {
-            P2CurrentShirt = shirts.Length - 1;
-        }
-        factory.ChangePlayerColor(ref p2, beards[P2CurrentBeard], shirts[P2CurrentShirt]);
-        StartCoroutine(SelectEffect(p2));
-    }
-
+    
     public IEnumerator SelectEffect(Player player)
     {
         float delta = 1.5f;
@@ -200,18 +121,152 @@ public class SetupView : MonoBehaviour
     //
     //              when Ready : not checking inputs 
 
+    #region Actions
+    #region ArrowActions
+    public void P1RightArrow()
+    {
+        //++P1CurrentShirt;
+        //P1CurrentShirt %= shirts.Length;
+        //factory.ChangePlayerColor(ref p1, beards[P1CurrentBeard], shirts[P1CurrentShirt]);
+        UIManager.Instance.players[0].indexShirt++;
+        UIManager.Instance.players[0].indexShirt %= shirts.Length;
+        factory.ChangePlayerColor(ref p1, beards[UIManager.Instance.players[0].indexBeard], shirts[UIManager.Instance.players[0].indexShirt]);
+
+        StartCoroutine(SelectEffect(p1));
+    }
+
+    public void P1LeftArrow()
+    {
+        //--P1CurrentShirt;
+        //UIManager.Instance.players[0].indexShirt
+        //if (P1CurrentShirt == -1)
+        //{
+        //    P1CurrentShirt = shirts.Length - 1;
+        //}
+        //factory.ChangePlayerColor(ref p1, beards[P1CurrentBeard], shirts[P1CurrentShirt]);
+        UIManager.Instance.players[0].indexShirt--;
+        if (UIManager.Instance.players[0].indexShirt == -1)
+        {
+            UIManager.Instance.players[0].indexShirt = shirts.Length - 1;
+        }
+        factory.ChangePlayerColor(ref p1, beards[UIManager.Instance.players[0].indexBeard], shirts[UIManager.Instance.players[0].indexShirt]);
+        StartCoroutine(SelectEffect(p1));
+    }
+
+    public void P2RightArrow()
+    {
+        //++P2CurrentShirt;
+        //P2CurrentShirt %= shirts.Length;
+        //factory.ChangePlayerColor(ref p2, beards[P2CurrentBeard], shirts[P2CurrentShirt]);
+        UIManager.Instance.players[1].indexShirt++;
+        UIManager.Instance.players[1].indexShirt %= shirts.Length;
+        factory.ChangePlayerColor(ref p2, beards[UIManager.Instance.players[1].indexBeard], shirts[UIManager.Instance.players[1].indexShirt]);
+        StartCoroutine(SelectEffect(p2));
+    }
+
+    public void P2LeftArrow()
+    {
+        //--P2CurrentShirt;
+        //if (P2CurrentShirt == -1)
+        //{
+        //    P2CurrentShirt = shirts.Length - 1;
+        //}
+        //factory.ChangePlayerColor(ref p2, beards[P2CurrentBeard], shirts[P2CurrentShirt]);
+
+        UIManager.Instance.players[1].indexShirt--;
+        if (UIManager.Instance.players[1].indexShirt == -1)
+        {
+            UIManager.Instance.players[1].indexShirt = shirts.Length - 1;
+        }
+        factory.ChangePlayerColor(ref p2, beards[UIManager.Instance.players[1].indexBeard], shirts[UIManager.Instance.players[1].indexShirt]);
+        StartCoroutine(SelectEffect(p2));
+    }
+    #endregion
+
+    private void Right_performed(InputAction.CallbackContext obj)
+    {
+        SwitchColor(obj.control.device.deviceId, DirectionType.Right);
+    }
+
+    private void Left_performed(InputAction.CallbackContext obj)
+    {
+        SwitchColor(obj.control.device.deviceId, DirectionType.Left);
+
+    }
+    
+
+    private void Submit_performed(InputAction.CallbackContext obj)
+    {
+        actionOver = true;
+        Debug.Log("Submit was held for :" + obj.duration + " duration");
+        if (obj.control.device.deviceId == (int)PlayerID.First )
+        {
+            UIManager.Instance.players[0].deviceId = 1;
+            UIManager.Instance.players[0].deviceName = obj.control.device.name;
+            viewport1.DisplayReady();
+        }
+        else if(obj.control.device.deviceId == (int)PlayerID.Second)
+        {
+            UIManager.Instance.players[1].deviceId = 2;
+            UIManager.Instance.players[1].deviceName = obj.control.device.name;
+            viewport2.DisplayReady();
+        }
+
+        if (viewport1.IsReady() && viewport2.IsReady())
+        {
+            SaveColors();
+            GoToNextPage();
+        }
+
+        //for debug purpose :
+        if (DEBUG_MODE)
+        {
+            SaveColors();
+            GoToNextPage();
+            DisplayAllPlayerGameData();
+        }
+    }
+    private void DisplayAllPlayerGameData()
+    {
+        foreach (var item in UIManager.Instance.players)
+        {
+            Debug.Log($"Player info : connected:{item.connected}, deviceId:{item.deviceId}, deviceName:{item.deviceName}, indexBeard:{item.indexBeard}, indexShirt:{item.indexShirt}, name:{item.name}");
+        }       
+    }
+    #endregion
+    private void SaveColors()
+    {
+        for (int i = 0; i < UIManager.Instance.players.Length; i++)
+        {
+            UIManager.Instance.players[i].indexBeard = players[i].indexBeard;
+            UIManager.Instance.players[i].indexShirt = players[i].indexShirt;
+        }
+    }
+
     private void LoadPlayers()
     {
         players = UIManager.Instance.players;
-        players[0].indexBeard = 0;
-        players[0].indexShirt = 0;
-        players[1].indexBeard = 1;
-        players[1].indexShirt = 3;
-        p1 = factory.CreatPlayer(beards[players[0].indexBeard], shirts[players[0].indexShirt]);
-        p2 = factory.CreatPlayer(beards[players[1].indexBeard], shirts[players[1].indexShirt]);
 
-        PlayerInput plInput1 = p1.gameObject.GetComponent<PlayerInput>();
-        PlayerInput plInput2 = p2.gameObject.GetComponent<PlayerInput>();
+        SetDefaultPlayerGameData();       
+
+        p1 = factory.CreatPlayer(beards[0], shirts[0]);
+        p2 = factory.CreatPlayer(beards[1], shirts[3]);
+        SetViewports();
+        
+        
+        //PlayerInput input1 = p1.gameObject.GetComponent<PlayerInput>();
+        //if(input1.devices[0].name == "Keyboard")
+        //{
+        //    //DisplayConnectedMsg(0);
+        //    dict[p1].DisplayConnecteMsg(DeviceType.Keyboard);
+        //}else if(input1.devices[0].name == "Controller")
+        //{
+        //    dict[p1].DisplayConnecteMsg(DeviceType.Controller);
+        //}
+        //Debug.Log("Player 1 is using " + input1.currentControlScheme);
+
+        //PlayerInput plInput2 = p2.gameObject.GetComponent<PlayerInput>();
+        //Debug.Log("Player 2 is using " + plInput2.currentControlScheme);
 
         p1.transform.position = p1SPawn.position;
         p1.transform.rotation = new Quaternion(0, 1, 0, 0);
@@ -222,16 +277,19 @@ public class SetupView : MonoBehaviour
         p2.transform.localScale = new Vector3(1.5f, 1.5f, 1.5f);
     }
 
-    private void LoadImages()
+    private void SetDefaultPlayerGameData()
     {
-        devicesSprites = Resources.LoadAll<Sprite>("Sprites/UI_Views/Devices");
+        UIManager.Instance.players[0].indexBeard = 0;
+        UIManager.Instance.players[0].indexShirt = 0;
+        UIManager.Instance.players[1].indexBeard = 1;
+        UIManager.Instance.players[1].indexShirt = 3;
     }
+   
 
     private void InitActions()
     {
         actions = new PlayerControls();
         actions.UI_Navigation.Submit.performed += Submit_performed;
-        actions.UI_Navigation.Submit.started += Submit_started;
         actions.UI_Navigation.Left.performed += Left_performed;
         actions.UI_Navigation.Right.performed += Right_performed; ;
         actions.UI_Navigation.Enable();
@@ -243,7 +301,6 @@ public class SetupView : MonoBehaviour
             {
                 case InputDeviceChange.Added:
                     // New Device.
-                    //if id correspond to a player, !assign it
                     for (int i = 0; i < UIManager.Instance.players.Length; i++)
                     {
                         if (UIManager.Instance.players[i].connected == false && UIManager.Instance.players[i].deviceId == 0)
@@ -293,6 +350,17 @@ public class SetupView : MonoBehaviour
         nextCanvas.gameObject.SetActive(true);
     }
 
+    private void SetViewports()
+    {
+        dict = new Dictionary<Player, PlayerViewport>();
+        dict.Add(p1, viewport1);
+        dict.Add(p2, viewport2);
+        //Assign the player field in viewport
+        dict[p1].LinkPlayer(p1);
+        dict[p2].LinkPlayer(p2);
+    }
 }
+
+
 
 
