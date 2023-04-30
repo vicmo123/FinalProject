3c0236d5b36a3ed72fba5f027df7dd55ed9b006c
Léonie St-Onge <leonie.stonge@gmail.com>
2023-04-30 00:31:54 -0400

    Changing image corresponding to input device

--- SugarShackGame/Assets/Scripts/UI/Views/SetupView.cs
+++ SugarShackGame/Assets/Scripts/UI/Views/SetupView.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.Layouts;
 using UnityEngine.UI;
-
+public enum DirectionType { Up, Down, Left, Right };
+public enum PlayerID { zero, First, Second };
 public class SetupView : MonoBehaviour
 {
-    private PlayerFactory factory;    
+    private PlayerFactory factory;
     private PlayerControls actions;
 
     #region Player
@@ -31,8 +33,17 @@ public class SetupView : MonoBehaviour
     public Button p1_rightArrowBtn;
     public Button p2_leftArrowBtn;
     public Button p2_rightArrowBtn;
+    private double holdDuration = 3f;
+    private double startTime = 0;
+    bool actionOver = false;
     #endregion
-    
+    #region ConnectedImg
+    public Image[] connectImg;
+    public TMP_Text[] texts;
+    public Image[] devices;
+    private Sprite[] devicesSprites;
+    #endregion
+
 
     private void Awake()
     {
@@ -40,58 +51,79 @@ public class SetupView : MonoBehaviour
         beards = factory.beardColors;
         shirts = factory.shirtColors;
         LoadPlayers();
+        LoadImages();
 
         actions = new PlayerControls();
-        actions.UI_Navigation.Down.performed += Down_performed;
-        actions.UI_Navigation.Up.performed += Up_performed;
         actions.UI_Navigation.Submit.performed += Submit_performed;
+        actions.UI_Navigation.Submit.started += Submit_started;
         actions.UI_Navigation.Left.performed += Left_performed;
-        actions.UI_Navigation.Right.performed += Right_performed; ; 
+        actions.UI_Navigation.Right.performed += Right_performed; ;
         actions.UI_Navigation.Enable();
     }
 
-    private void Right_performed(InputAction.CallbackContext obj)
+    private void Submit_started(InputAction.CallbackContext obj)
     {
-        if (obj.control.device.deviceId == 1)
-        {
-            P1RightArrow();
-        }
-        if (obj.control.device.deviceId == 3)
-        {
-            P2RightArrow();
-        }
-    }
-
-    private void Left_performed(InputAction.CallbackContext obj)
-    {
-        if (obj.control.device.deviceId == 1)
-        {
-            P1LeftArrow();
-        }
-        if(obj.control.device.deviceId == 3)
-        {
-            P2LeftArrow();
-        }
+        actionOver = false;
+        startTime = obj.startTime;
     }
 
     private void Submit_performed(InputAction.CallbackContext obj)
     {
-        
-        Debug.Log("Submit" + obj.control.device.deviceId);
+        actionOver = true;
+        if (obj.control.device.deviceId == (int)PlayerID.First && obj.duration <= holdDuration)
+        {
+            connectImg[0].color = new Color(0.237f, 0.99f, 0.088f, 0.63f);
+            texts[0].text = "Connected";
+            if (obj.control.device.name != "Keyboard" &&  devicesSprites[0].name == "Controller")
+                devices[0].sprite = devicesSprites[0];
+            else
+            {
+                devices[0].sprite = devicesSprites[1];
+            }
+            Debug.Log("Submit" + obj.control.device.deviceId);
+        }
+        if (obj.control.device.deviceId == (int)PlayerID.Second && obj.duration <= holdDuration)
+        {
+            connectImg[1].color = new Color(0.87f, 0.00f, 0.98f, 0.01f);
+            connectImg[1].GetComponentInChildren<TextMeshPro>().text = "Connected";
+        }
     }
 
-    private void Up_performed(InputAction.CallbackContext obj)
+    private void Right_performed(InputAction.CallbackContext obj)
     {
-
-        Debug.Log("up");
+        SwitchColor(obj.control.device.deviceId, DirectionType.Right);
     }
 
-    private void Down_performed(InputAction.CallbackContext obj)
+    private void Left_performed(InputAction.CallbackContext obj)
     {
-        Debug.Log("down from : " + obj.valueType.GUID);
-    }
-    
+        SwitchColor(obj.control.device.deviceId, DirectionType.Left);
 
+    }
+    private void SwitchColor(int id, DirectionType type)
+    {
+        switch (type)
+        {
+            case DirectionType.Left:
+                if (id == (int)PlayerID.First)
+                    P1LeftArrow();
+                else
+                    P2LeftArrow();
+
+                break;
+            case DirectionType.Right:
+                if (id == (int)PlayerID.First)
+                    P1RightArrow();
+                else
+                    P2RightArrow();
+
+                break;
+            default:
+                break;
+        }
+        Player player = (id == (int)PlayerID.First) ? p1 : p2;
+        StartCoroutine(SelectEffect(player));
+
+    }
 
     public void P1RightArrow()
     {
@@ -149,7 +181,7 @@ public class SetupView : MonoBehaviour
     //			  5. State of player :  {Setup, Ready}
     //
     //              when Ready : not checking inputs 
-  
+
     private void LoadPlayers()
     {
         p1 = factory.CreatPlayer(beards[0], shirts[0]);
@@ -167,6 +199,14 @@ public class SetupView : MonoBehaviour
         p2.transform.localScale = new Vector3(1.5f, 1.5f, 1.5f);
     }
 
-   
+    private void LoadImages()
+    {
+        devicesSprites = Resources.LoadAll<Sprite>("Sprites/UI_Views/Devices");
+    }
+}
+
+public class PlayerViewport
+{
+
 }
 
