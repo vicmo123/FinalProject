033fa29d28cb19dfeef88ab21609cc50397bb439
Léonie St-Onge <leonie.stonge@gmail.com>
2023-05-10 09:56:46 -0400

    Multiplayer Input

---
+++ SugarShackGame/Assets/Scripts/TEST/Test_Multiplayer_Input.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using System.Linq;
+using UnityEngine.SceneManagement;
+
+public class Test_Multiplayer_Input : MonoBehaviour
+{
+
+}
