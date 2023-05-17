d256f43ffd7850f2ef31c2929467962c54ed365b
vicmo123 <vicmo26@gmail.com>
2023-05-17 16:58:16 -0400

    Shield is done
    - Prefabs appear on scene and protects the player for a certain period of time

--- SugarShackGame/Assets/Scripts/Abilities/AbilityComponents/Shield.cs
+++ SugarShackGame/Assets/Scripts/Abilities/AbilityComponents/Shield.cs
@@ -10,13 +10,16 @@ public class Shield : UnThrowableAbility
     public override void InitAbility(Ability _stats, Player _player)
     {
         base.InitAbility(_stats, _player);
-        //MakeEffect();
+
+        MakeEffect();
     }
 
     public override void MakeEffect()
     {
-        var iceSphere = GameObject.Instantiate(ShieldPrefab);
-        iceSphere.GetComponent<ShieldEffect>().MakeShieldEffect(ShieldEffectDuration, player);
-        readyToBeDestroyed = true;
+        base.MakeEffect();
+
+        GameObject shieldObj = Instantiate<GameObject>(ShieldPrefab);
+        var effect = shieldObj.GetComponent<ShieldEffect>();
+        effect.ProtectPlayer(player, ShieldEffectDuration);
     }
 }
